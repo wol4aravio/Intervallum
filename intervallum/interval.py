490b44647244960dd2136aa1605eba1d9e08a6f1
Valentin Panovskiy <wol4aravio@yandex.ru>
2019-01-29 08:52:47 +0300

    first steps

--- intervallum/interval.py
+++ intervallum/interval.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Interval:
 
     _reduction_width: float = 1e-5
@@ -5,11 +7,10 @@ class Interval:
     __slots__ = ["lb", "ub"]
 
     def __init__(self, lower_bound: float, upper_bound: float, reduce: bool = True):
-        if reduce and (upper_bound - lower_bound) > Interval._reduction_width:
+        if reduce and (upper_bound - lower_bound) < Interval._reduction_width:
             middle = 0.5 * (lower_bound + upper_bound)
             self.lb = middle
             self.ub = middle
         else:
             self.lb = lower_bound
             self.ub = upper_bound
-
